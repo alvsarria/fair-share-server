b54b324cdc7a3d0c55a0909b054104cc2b8a4fac
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-06-02 19:21:09 +0200

    adding user routes and updating user model to allow profile pictures

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,16 +2,35 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-// // importing Group.model
-// const Expense = require("../models/Expense.model");
-// const Group = require("../models/Group.model")
+// importing User.model
+const User = require("../models/User.model")
 
-// // Defining initial request for gome, GET all projects
-// router.get("/home", (req, res, next) => {
-//   Group.find()
-//   .populate("expenses users")
-//     .then((allGroups) => res.json(allGroups))
-//     .catch((err) => res.json(err));
-// });
+// Gets user details
+router.get("/:userId", (req, res, next) => {
+    const {userId} = req.params;
+    User.findById(userId)
+        // .populate("expenses users")
+        .then((User) => res.json(User))
+        .catch((error) => res.json(error));
+});
 
-// module.exports = router;
+// Updates user details
+router.put("/:userId", (req, res, next) => {
+    const {userId} = req.params;
+
+    if(req.body.name === "" | req.body.lastName === "" | req.body.dateOfBirth === "" | req.body.phoneNumber === "" | req.body.email === "" ){
+        res.status(400).json({message: "Please fill all the fields"})
+        return;
+    };
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        res.status(400).json({ message: "Specified id is not valid" });
+        return;
+      };
+
+    User.findByIdAndUpdate(userId, req.body, {new: true})
+    .then(() => res.status(202).json({message: "User updated!"}))
+    .catch((error) => res.json(error));
+})
+
+module.exports = router;
